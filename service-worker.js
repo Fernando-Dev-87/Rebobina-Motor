49e8a2cf43a52e6824ff3e25b904fe216298c40f
Fernando Dev <rebobinamotor@example.com>
2026-09-18 22:33:30 -0300

    Organize PWA files for easy GitHub Pages access

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'rebobina-motor-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './styles.css',
+  './js/app.js',
+  './icons/icon-192x192.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
